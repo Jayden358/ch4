cbab13ea027706b2b33046f6deed95e35f21b3ac
jayden.williams <jayden.williams@CLC-LAB209-17.TCSD.tooelesd.org>
2018-10-17 13:10:33 -0600

    a

---
+++ ch4/src/ch4/FindFutureDates.java
@@ -0,0 +1,25 @@
+package ch4;
+
+import java.util.Scanner;
+
+public class FindFutureDates {
+
+	public static void main(String[] args) {
+		//ints
+		int Sunday=0;
+		int Monday=1;
+		int Tuesday=2;
+		int Wednesday=3;
+		int Thursday=4;
+		int Friday=5;
+		int Saturday=6;
+		int today;
+		int elapsed;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter Today's day: ");
+		today = input.nextInt();
+		//must finish
+
+	}
+
+}
